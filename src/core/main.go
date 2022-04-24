866251587176f3756c9c23a808c2dfb7efd98f33
刘书鑫 <642545320@qq.com>
2022-04-25 00:28:13 +0800

    架构设计，头像及名片设置

--- src/core/main.go
+++ src/core/main.go
@@ -1,1 +1,13 @@
-package core
+package main
+
+import (
+	"fmt"
+	"originalGod/src/game"
+)
+
+func main() {
+	player := game.NewTestPlayer()
+	fmt.Println(player.ModIdcard)
+	player.RecvSetIcon(1)   // 胡桃icon
+	player.RecvSetIdcard(2) // 枫叶
+}
