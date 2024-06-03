e6f12c995c6dbc23965aa2ee66588bfef033be1e
lsx_hp_win <642545320@qq.com>
2024-06-03 17:57:57 +0800

    多线程与数据安全

--- src/game/mod_uniquetask.go
+++ src/game/mod_uniquetask.go
@@ -1,5 +1,7 @@
 package game
 
+import "sync"
+
 /*唯一任务模块 该模块在一个账号的生命周期内只能完成一次*/
 
 type TaskInfo struct {
@@ -8,11 +10,14 @@ type TaskInfo struct {
 }
 
 type ModUniqueTask struct {
-	MyTaskInfo map[int]*TaskInfo
+	MyTaskInfo map[int]*TaskInfo // 任务map 一定会有多线程来并发读写的
+	Locker     sync.RWMutex
 }
 
 // IsTaskCompleted 任务是否完成
 func (mt *ModUniqueTask) IsTaskCompleted(taskId int) bool {
+	mt.Locker.RLocker()
+	defer mt.Locker.RUnlock()
 	task, ok := mt.MyTaskInfo[taskId]
 	if !ok {
 		return false
